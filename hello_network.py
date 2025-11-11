2fa9b767993a319000210502f2d7f1fcef707508
tmorri5077 <47067832+tmorri5077@users.noreply.github.com>
2025-11-11 10:00:55 -0500

    Fix typo in print statement message

--- hello_network.py
+++ hello_network.py
@@ -2,5 +2,5 @@ def get_device(device name):
     return f"Device {device_name} is up and reachable via API."
 
 if__name__== "__main__":
-print("Hello DevNet, practicing Git pull from GitHub!")
+print("Hello DevNet, practicing Git pulling from GitHub!")
 print(get_device_status("core-switch-01"))
