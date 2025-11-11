e952ece3c0b4a10f26fbff215b7569864311af80
tmorri5077 <morrisontaneasha@gmail.com>
2025-11-11 09:50:07 -0500

    Add hello_netwrok DevNet practice script

---
+++ hello_network.py
@@ -0,0 +1,6 @@
+def get_device(device name):
+    return f"Device {device_name} is up and reachable via API."
+
+if__name__== "__main__":
+print("Hello DevNet, from Git & Git:Hub practice!")
+print(get_device_status("core-switch-01"))
